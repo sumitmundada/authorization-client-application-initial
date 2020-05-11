4b2d694135e258081f4b7cc3562e5cac300bc673
Spyro1001 <raymondhedgecock@gmail.com>
2020-05-11 16:25:08 -0700

    Initial commit

---
+++ src/test/java/com/example/oauthclient/AuthorizationClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.oauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class AuthorizationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
